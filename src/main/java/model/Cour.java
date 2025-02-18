b3450e05d8e86725a8be1806795df768e059a819
iliassid <enaailiass@gmail.com>
2025-02-18 12:39:03 +0100

    added class cour

--- src/main/java/model/Cour.java
+++ src/main/java/model/Cour.java
@@ -1,4 +1,29 @@
 package model;
 
 public class Cour {
+
+    private String courName;
+    private String description;
+
+    public Cour(String courName, String description) {
+        this.courName = courName;
+        this.description = description;
+    }
+
+    public String getCourName() {
+        return courName;
+    }
+
+    public void setCourName(String courName) {
+        this.courName = courName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
