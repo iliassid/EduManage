162ddd8d176df5169f203d850602b82b7a17d551
fatimaezzahraadyani <bouhlalamine01@gmail.com>
2025-02-18 16:24:31 +0100

    diagramme de cas d'utilisation uml + class etetudiant + page create.jsp

--- src/main/java/model/Etudiant.java
+++ src/main/java/model/Etudiant.java
@@ -1,4 +1,57 @@
 package model;
 
 public class Etudiant {
+    private int id;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String naissance;
+
+    public Etudiant(int id, String prenom, String nom, String email, String naissance) {
+        this.id = id;
+        this.prenom = prenom;
+        this.nom = nom;
+        this.email = email;
+        this.naissance = naissance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNaissance() {
+        return naissance;
+    }
+
+    public void setNaissance(String naissance) {
+        this.naissance = naissance;
+    }
 }
